dd0f358966f29aaa863d521157f2e6f031616697
nsingh308 <navdeep.singh.kanwal@gmail.com>
2019-04-04 20:51:34 +0530

    nested components added. command$:ng generate component componentName

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './successAlert/successAlert.component';
+import {WarningAlertComponent} from './warningAlert/warningAlert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule
